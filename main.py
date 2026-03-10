93e0d17643d50b76b220399b4ff3118febfd3ed8
Bartholomeo <143606281+Bartholomovic@users.noreply.github.com>
2026-03-10 11:20:00 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T08:09:56.246Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-10T08:19:59.472Z
+Commit: Fix CSS styling bug on mobile devices
