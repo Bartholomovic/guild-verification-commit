3dd8b378b189e5e78742c1b347f600272962b028
Bartholomeo <143606281+Bartholomovic@users.noreply.github.com>
2026-03-10 11:10:08 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T08:09:32.823Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-10T08:10:07.079Z
+Commit: Patch API endpoint error handling
