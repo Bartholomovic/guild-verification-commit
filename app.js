59b0e1f2c952cbb1faed93bbcccb267b0e1b1b30
Bartholomeo <143606281+Bartholomovic@users.noreply.github.com>
2026-03-10 11:09:18 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T08:07:05.183Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-10T08:09:17.623Z
+Commit: Resolve null pointer exception in user service
