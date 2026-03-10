91bf45af5e5f4898eccfdd920b3ceb907aecb3e4
Bartholomeo <143606281+Bartholomovic@users.noreply.github.com>
2026-03-10 11:08:12 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T08:08:09.368Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-10T08:08:11.455Z
+Commit: Fix CSS styling bug on mobile devices
